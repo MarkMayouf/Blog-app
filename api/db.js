f1977b6933bb25d2716aaa7afc07c436e70a6e4d
MarkMa <102841858+MarkMayouf@users.noreply.github.com>
2025-04-18 14:49:43 -0400

    Update db.js

--- api/db.js
+++ api/db.js
@@ -17,6 +17,5 @@ export const db = mysql.createConnection({
   database: process.env.DB_NAME || "blog",
   port: process.env.DB_PORT,
 });
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASS exists:", !!process.env.DB_PASS);
+
 
