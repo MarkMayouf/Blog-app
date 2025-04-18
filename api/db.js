e1130e7e0c2a58af3ca467bfc7ffeae7c4db8d38
Akram Mayouf <akramkarl38@gmail.com>
2025-04-18 09:58:47 -0400

    updated

--- api/db.js
+++ api/db.js
@@ -14,6 +14,7 @@ export const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || "blog"
+  database: process.env.DB_NAME || "blog",
+  port: process.env.DB_PORT,
 });
 
